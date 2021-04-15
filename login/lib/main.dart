7d97f5fd6a14479e953034a822fe5b19e06238d5
Jean Tan Li <jeantl@outlook.com.br>
2021-04-15 16:33:36 -0300

    Inicializando firebase na main

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login/screens/login_screen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
+Future<void> main() async {
+  //Inicialização do firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
