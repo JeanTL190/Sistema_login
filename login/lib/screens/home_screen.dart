56f9e92785652c345605786089c6c8154ed3aab9
Jean Tan Li <jeantl@outlook.com.br>
2021-04-16 19:51:33 -0300

    Centralizar tela home

--- login/lib/screens/home_screen.dart
+++ login/lib/screens/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
           );
         else {
           return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text("Logado !"),
               ElevatedButton(
